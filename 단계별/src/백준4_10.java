3c547f504714ccefa1519a36bc2b72b85f9d337a
yooniverse7 <tpdbs0701@gmail.com>
2024-04-01 20:35:20 +0900

    1546

---
+++ 단계별/src/백준4_10.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class 백준4_10 {
+    
+    public static void main(String[] args) {
+        
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int[] arr = new int[n];
+
+        //값 입력받기
+        for (int i = 0; i < n; i++) {
+            arr[i] = s.nextInt();
+        }
+
+        //최대값 구하기
+        int max = arr[0];
+        for(int i = 1; i<n; i++) {
+            if(arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        float avg = 0;
+
+        //평균구해서 출력하기
+        for(int j = 0; j<n; j++) {
+            avg += (float)arr[j]/max*100; //나눗셈 연산 시 실수로 나오고 싶다면 실수형 붙여줘야함
+        }
+        avg/=n;
+        System.out.println(avg);
+        s.close();
+
+
+    }
+}
